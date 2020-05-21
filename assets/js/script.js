9e82300c2367d89a758632ee4f98dddf135cf254
ZQWhiting <zach.whiting@icloud.com>
2020-05-21 11:46:13 -0600

    class name cleanup

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// h-btn functionality should take player to the high score page and if in the middle of the quiz? should end ask if player wishes to end quiz.
+
+// time counter should count down from 75 every second
+
+// start page should be centered
+// start game btn should remove all content in the content-holder, remove center alignment, and load first question and the answer options that correspond to that question
+
+// questions and answers should be single objects
+// question objects should be organized in an array
+
+// all buttons should remove all content and load new content
+
+// the form should submit the userInput + time as an object to an array that will be saved into LocalStorage for highscores
+
+// Load Game
